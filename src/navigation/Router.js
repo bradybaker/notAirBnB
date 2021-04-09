dd68050ca0c58cc4d18a9d613ebbe85136a7fcfd
Brady Baker <bradybaker82@gmail.com>
2021-04-09 15:54:16 -0500

    bottom nav added

--- src/navigation/Router.js
+++ src/navigation/Router.js
@@ -2,6 +2,8 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
 import DestinationSearchScreen from '../screens/DestinationSearch';
+import GuestScreen from '../screens/Guests'
+import HomeTabNavigator from './HomeTabNavigator'
 
 const Stack = createStackNavigator();
 
@@ -9,7 +11,9 @@ const Router = () => {
     return (
         <NavigationContainer>
             <Stack.Navigator>
-                <Stack.Screen name={"Destination Search"} component={DestinationSearchScreen} />
+                <Stack.Screen name={"Home"} component={HomeTabNavigator} options={{headerShown: false}} />
+                {/* <Stack.Screen name={"Destination Search"} component={DestinationSearchScreen} options={{title: "Search A Destination"}} />
+                <Stack.Screen name={"Guests"} component={GuestScreen} options={{title: "How Many People?"}} /> */}
             </Stack.Navigator>
         </NavigationContainer>
     )
