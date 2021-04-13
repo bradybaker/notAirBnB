6178a409054f00401a173b88bde63e34d0e61f90
Brady Baker <bradybaker82@gmail.com>
2021-04-13 14:39:36 -0500

    Added google places autocomplete to destination search

--- src/screens/DestinationSearch/index.js
+++ src/screens/DestinationSearch/index.js
@@ -1,18 +1,40 @@
 import React, { useState }from 'react';
-import {View, TextInput, FlatList, Text, Pressable} from 'react-native';
+import {View, TextInput, FlatList, Text, Pressable, processColor} from 'react-native';
 import styles from './styles';
 import searchResults from '../../../assets/data/search';
-import Entypo from 'react-native-vector-icons/Entypo'
-import { useNavigation } from '@react-navigation/native'
-
+import Entypo from 'react-native-vector-icons/Entypo';
+import { useNavigation } from '@react-navigation/native';
+import { GooglePlacesAutocomplete } from 'react-native-google-places-autocomplete'
+import Config from 'react-native-config'
+ 
 const DestinationSearchScreen = () => {
     
     const navigation = useNavigation();
     const [inputText, setInputText] = useState('');
+    const API_KEY = Config.GOOGLE_API_KEY
 
     return (
         <View style={styles.container}>
           {/* Input */}
+          <View style={{height: 500}}>
+              <GooglePlacesAutocomplete
+            placeholder='Where are you going?'
+            onPress={(data, details = null) => {
+              // 'details' is provided when fetchDetails = true
+              console.log(data, details);
+            }}
+            query={{
+              key: `${API_KEY}`,
+              language: 'en',
+              types: '(cities)',
+            }}
+          />
+          </View>
+
+
+          
+
+
             <TextInput 
               style={styles.textInput}
               placeholder="Where are you going?"
